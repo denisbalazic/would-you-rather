6160701330f01b84cdceb38538905636845241c0
denisbalazic <denisbalazic42@gmail.com>
2021-01-15 18:48:18 +0100

    feat: Connect QuestionsList component to Redux store
    Connect QuestionList component to Redux store's questions and
    authedUser objects. Pass questions props to Question component.
    Render question props in Question component.

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 
 export default class Question extends Component {
   render() {
-    return <div>Question</div>;
+    const { id, author, optionOne, optionTwo } = this.props.question;
+    return (
+      <div>
+        Question:
+        <p>{author}</p>
+        <p>{optionOne.text}</p>
+        <p>{optionTwo.text}</p>
+        <p>{id}</p>
+      </div>
+    );
   }
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Question from "./Question";
 
-export default class QuestionList extends Component {
+class QuestionList extends Component {
   render() {
+    const { questions, authedUser } = this.props;
     return (
       <div>
-        QuestionList
-        <Question />
+        QuestionList:
+        {questions.map((question) => (
+          <Question key={question.id} question={question} />
+        ))}
       </div>
     );
   }
 }
+
+function mapStateToProps({ questions, authedUser }) {
+  return {
+    questions: Object.values(questions).sort((a, b) => b.timestamp - a.timestamp),
+    authedUser,
+  };
+}
+
+export default connect(mapStateToProps)(QuestionList);
