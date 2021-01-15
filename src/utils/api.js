ce010a1698ea982f640ae60f238760f6c0359c40
denisbalazic <denisbalazic42@gmail.com>
2021-01-15 13:31:50 +0100

    Add api calls to dummy database

---
+++ src/utils/api.js
@@ -0,0 +1,7 @@
+import { _getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer } from "../utils/data";
+
+export function getInitialData() {
+  return Promise.all([_getQuestions(), _getUsers()]).then(([questions, users]) => {
+    questions, users;
+  });
+}
