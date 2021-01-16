83827693a421ef898adda2fee233e3f608e29e49
denisbalazic <denisbalazic42@gmail.com>
2021-01-16 16:04:24 +0100

    feat: Change authedUser store to accept whole user

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,14 @@
+import { getUser } from "../utils/api";
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
 
+// Ask if this is OK????
 export function setAuthedUser(id) {
-  return {
-    type: SET_AUTHED_USER,
-    id,
+  return (dispatch) => {
+    return getUser(id).then((user) => {
+      dispatch({
+        type: SET_AUTHED_USER,
+        user,
+      });
+    });
   };
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,18 @@
-import { _getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer } from "../utils/data";
+import {
+  _getQuestions,
+  _getUsers,
+  _getUser,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from "../utils/data";
 
 export function getInitialData() {
   return Promise.all([_getQuestions(), _getUsers()]).then(([questions, users]) => ({
     questions,
     users,
   }));
+}
+
+export function getUser(id) {
+  return _getUser(id).then((user) => user);
 }
