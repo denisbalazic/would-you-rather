a0a91a1acc2dcba9bdca1f2f3e400f6777e4dabd
denisbalazic <denisbalazic42@gmail.com>
2021-01-19 17:22:23 +0100

    refactor: Extract function from render

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Question from "./Question";
 
+//filter questions based on showAnswered state and authedUser answers
+function selectQuestions(questions, answered, showAnswered) {
+  if (questions.length > 0 && answered.length > 0) {
+    return questions
+      .filter((q) => {
+        if (showAnswered) {
+          return answered.indexOf(q.id) >= 0;
+        } else {
+          return answered.indexOf(q.id) < 0;
+        }
+      })
+      .sort((a, b) => b.timestamp - a.timestamp);
+  } else {
+    return [];
+  }
+}
+
 class QuestionList extends Component {
   state = {
     showAnswered: false,
@@ -13,19 +30,8 @@ class QuestionList extends Component {
     this.setState({ showAnswered: false });
   };
   render() {
-    const { questions, authedUser } = this.props;
-    let selectedQuestions = [];
-    if (Object.keys(questions).length > 0 && authedUser !== null) {
-      selectedQuestions = Object.values(questions).filter((q) => {
-        //filter questions based on showAnswered state and authedUser answers
-        if (this.state.showAnswered) {
-          return Object.keys(authedUser.answers).indexOf(q.id) >= 0;
-        } else {
-          return Object.keys(authedUser.answers).indexOf(q.id) < 0;
-        }
-      });
-      selectedQuestions.sort((a, b) => b.timestamp - a.timestamp);
-    }
+    const { questions, answered } = this.props;
+    const selectedQuestions = selectQuestions(questions, answered, this.state.showAnswered);
     return (
       <div className="questionlist">
         <button onClick={this.showUnanswered}>Unanswered</button>
@@ -40,8 +46,8 @@ class QuestionList extends Component {
 
 function mapStateToProps({ questions, authedUser }) {
   return {
-    questions,
-    authedUser,
+    questions: Object.values(questions),
+    answered: authedUser ? Object.keys(authedUser.answers) : [],
   };
 }
 
