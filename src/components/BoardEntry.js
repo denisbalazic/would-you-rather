0bf713478e4910729a0ba2d1dd279e30de6cebd0
denisbalazic <denisbalazic42@gmail.com>
2021-01-26 09:09:45 +0100

    feat: Add avatar to leaderboard entries

--- src/components/BoardEntry.js
+++ src/components/BoardEntry.js
@@ -3,20 +3,21 @@ import PropTypes from "prop-types";
 
 export default class BoardEntry extends Component {
   render() {
-    const { name, questions, answers, avatarUrl } = this.props.user;
+    const { name, questions, answers, avatarURL } = this.props.user;
     return (
       <div className="score-card">
-        {/* <p>{avatarUrl}</p> */}
+        <div className="avatar">
+          <img src={avatarURL} alt="" />
+        </div>
         <p className="name">{name}</p>
         <p className="score">
           <small>asked </small>
-          {questions.length}
+          {questions.length} /
         </p>
         <p className="score">
-          /<small>answered </small>
+          <small> answered </small>
           {Object.keys(answers).length}
         </p>
-        <p className="score-sum">/{questions.length + Object.keys(answers).length}</p>
       </div>
     );
   }
